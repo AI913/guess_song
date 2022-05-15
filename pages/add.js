410b6e10b5082a9150afef30ae083ece7062b52b
AI913 <dywchiu913@hotmail.com>
2022-05-15 21:47:29 +0900

    nav bar and little styles

--- pages/add.js
+++ pages/add.js
@@ -1,11 +1,11 @@
 import SongForm from '../lib/SongForm'
 import styles from '../styles/Home.module.css'
-import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1>出題目啦！</h1>
+      <SongForm />
     </div>
   )
 }
