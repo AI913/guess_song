01e0e334497a52d6c058230e2951cee047ec31ed
AI913 <dywchiu913@hotmail.com>
2023-04-30 02:37:56 +0900

    display the lyrics of the song in question(random)

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,29 +2,51 @@ import SongForm from '../lib/SongForm'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link';
 import firebase from "../firebase/clientApp"
+import { getDocs, addDoc, collection } from "firebase/firestore"
+import { useState, useEffect } from 'react';
+
 // Import the useAuthStateHook
 import { useAuthState } from "react-firebase-hooks/auth";
-import { useCollection } from "react-firebase-hooks/firestore";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { read } from 'fs';
 
 export default function Home() {
 
+  const db = firebase.firestore();
+
   // Destructure user, loading, and error out of the hook.
-  const [user, loading, error] = useAuthState(firebase.auth());
+  const [user, loading, error] = useAuthState(firebase.auth() as any);
 
   console.log("Loading:", loading, "|", "Current user:", user);
 
-  const [songs, songsLoading, songsError] = useCollection(
-      firebase.firestore().collection("songs"),
-      {}
-  );
+  const [randomSong, setRandomSong] = useState(null);
 
-  if (!songsLoading && songs) {
-      songs.docs.map((doc) => console.log(doc.data()));
-  }
+  useEffect(() => {
+    const collectionRef = db.collection('songs');
+
+    const randomId = collectionRef.doc().id;
+
+    collectionRef
+    .orderBy('__name__')
+    .endBefore(randomId)
+    .limit(1)
+    .get()
+    .then(snapshot => {
+      snapshot.forEach(doc => {
+        console.log(doc.data());
+        const data = doc.data();
+        setRandomSong(data.lyrics)
+        console.log(randomId);
+      });
+    });
+
+  }, [])
 
   return (
     <div className={styles.container}>
-      <h1>出題目啦！</h1>
+      <h1>
+      {randomSong}
+      </h1>
       <form>
           <input name="title" type="text" />
       </form>
