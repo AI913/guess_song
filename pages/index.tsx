42a1d55c7d8d73d7ca58a38778dea83faad5d15e
AI913 <dywchiu913@hotmail.com>
2023-05-05 23:37:46 +0900

    changed names of constants to fit with new two-part lyrics data structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,16 @@
-import SongForm from '../lib/SongForm'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link';
-import firebase from "../firebase/clientApp"
-import { getDocs, addDoc, collection } from "firebase/firestore"
-import { useState, useEffect } from 'react';
+import SongForm from "../lib/SongForm";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import firebase from "../firebase/clientApp";
+import { getDocs, addDoc, collection } from "firebase/firestore";
+import { useState, useEffect } from "react";
 
 // Import the useAuthStateHook
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollectionData } from "react-firebase-hooks/firestore";
-import { read } from 'fs';
+import { read } from "fs";
 
 export default function Home() {
-
   const db = firebase.firestore();
 
   // Destructure user, loading, and error out of the hook.
@@ -19,37 +18,34 @@ export default function Home() {
 
   console.log("Loading:", loading, "|", "Current user:", user);
 
-  const [randomSong, setRandomSong] = useState(null);
+  const [randomLyrics1, setRandomLyrics1] = useState(null);
 
   useEffect(() => {
-    const collectionRef = db.collection('songs');
+    const collectionRef = db.collection("songs");
 
     const randomId = collectionRef.doc().id;
 
     collectionRef
-    .orderBy('__name__')
-    .startAt(randomId)
-    .limit(1)
-    .get()
-    .then(snapshot => {
-      snapshot.forEach(doc => {
-        console.log(doc.data());
-        const data = doc.data();
-        setRandomSong(data.lyrics)
-        console.log(randomId);
+      .orderBy("__name__")
+      .startAt(randomId)
+      .limit(1)
+      .get()
+      .then((snapshot) => {
+        snapshot.forEach((doc) => {
+          console.log(doc.data());
+          const data = doc.data();
+          setRandomLyrics1(data.lyrics1);
+          console.log(randomId);
+        });
       });
-    });
-
-  }, [])
+  }, []);
 
   return (
     <div className={styles.container}>
-      <h1>
-      {randomSong}
-      </h1>
+      <h1>{randomLyrics1}</h1>
       <form>
-          <input name="title" type="text" />
+        <input name="title" type="text" />
       </form>
     </div>
-  )
+  );
 }
